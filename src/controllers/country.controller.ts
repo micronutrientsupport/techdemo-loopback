8a87932e09415276fabe4b0550ca7b3ef81a2f0f
jonstu <jonstu@bgs.ac.uk>
2020-10-16 09:11:14 +0100

    Remove the generated rest endpoints that we don't want.  This is a dictionary so we just want the "get all" endpoint.

--- src/controllers/country.controller.ts
+++ src/controllers/country.controller.ts
@@ -1,20 +1,11 @@
 import {
-  Count,
-  CountSchema,
   Filter,
-  FilterExcludingWhere,
-  repository,
-  Where,
+
+  repository
 } from '@loopback/repository';
 import {
-  post,
-  param,
   get,
-  getModelSchemaRef,
-  patch,
-  put,
-  del,
-  requestBody,
+  getModelSchemaRef, param
 } from '@loopback/rest';
 import {Country} from '../models';
 import {CountryRepository} from '../repositories';
@@ -22,46 +13,8 @@ import {CountryRepository} from '../repositories';
 export class CountryController {
   constructor(
     @repository(CountryRepository)
-    public countryRepository : CountryRepository,
+    public countryRepository: CountryRepository,
   ) {}
-
-  @post('/countries', {
-    responses: {
-      '200': {
-        description: 'Country model instance',
-        content: {'application/json': {schema: getModelSchemaRef(Country)}},
-      },
-    },
-  })
-  async create(
-    @requestBody({
-      content: {
-        'application/json': {
-          schema: getModelSchemaRef(Country, {
-            title: 'NewCountry',
-            exclude: ['id'],
-          }),
-        },
-      },
-    })
-    country: Omit<Country, 'id'>,
-  ): Promise<Country> {
-    return this.countryRepository.create(country);
-  }
-
-  @get('/countries/count', {
-    responses: {
-      '200': {
-        description: 'Country model count',
-        content: {'application/json': {schema: CountSchema}},
-      },
-    },
-  })
-  async count(
-    @param.where(Country) where?: Where<Country>,
-  ): Promise<Count> {
-    return this.countryRepository.count(where);
-  }
 
   @get('/countries', {
     responses: {
@@ -84,90 +37,4 @@ export class CountryController {
     return this.countryRepository.find(filter);
   }
 
-  @patch('/countries', {
-    responses: {
-      '200': {
-        description: 'Country PATCH success count',
-        content: {'application/json': {schema: CountSchema}},
-      },
-    },
-  })
-  async updateAll(
-    @requestBody({
-      content: {
-        'application/json': {
-          schema: getModelSchemaRef(Country, {partial: true}),
-        },
-      },
-    })
-    country: Country,
-    @param.where(Country) where?: Where<Country>,
-  ): Promise<Count> {
-    return this.countryRepository.updateAll(country, where);
-  }
-
-  @get('/countries/{id}', {
-    responses: {
-      '200': {
-        description: 'Country model instance',
-        content: {
-          'application/json': {
-            schema: getModelSchemaRef(Country, {includeRelations: true}),
-          },
-        },
-      },
-    },
-  })
-  async findById(
-    @param.path.string('id') id: string,
-    @param.filter(Country, {exclude: 'where'}) filter?: FilterExcludingWhere<Country>
-  ): Promise<Country> {
-    return this.countryRepository.findById(id, filter);
-  }
-
-  @patch('/countries/{id}', {
-    responses: {
-      '204': {
-        description: 'Country PATCH success',
-      },
-    },
-  })
-  async updateById(
-    @param.path.string('id') id: string,
-    @requestBody({
-      content: {
-        'application/json': {
-          schema: getModelSchemaRef(Country, {partial: true}),
-        },
-      },
-    })
-    country: Country,
-  ): Promise<void> {
-    await this.countryRepository.updateById(id, country);
-  }
-
-  @put('/countries/{id}', {
-    responses: {
-      '204': {
-        description: 'Country PUT success',
-      },
-    },
-  })
-  async replaceById(
-    @param.path.string('id') id: string,
-    @requestBody() country: Country,
-  ): Promise<void> {
-    await this.countryRepository.replaceById(id, country);
-  }
-
-  @del('/countries/{id}', {
-    responses: {
-      '204': {
-        description: 'Country DELETE success',
-      },
-    },
-  })
-  async deleteById(@param.path.string('id') id: string): Promise<void> {
-    await this.countryRepository.deleteById(id);
-  }
 }
